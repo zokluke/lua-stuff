4c511e94eb6a759fff116f496d3d6665ebbe963c
Luke <68570779+zokluke@users.noreply.github.com>
2021-05-15 12:50:08 -0500

    Create translate-all-chat-to-english.lua

---
+++ translate-all-chat-to-english.lua
@@ -0,0 +1,25 @@
+local language = "en" -- works with 'en' (english), 'es' (spanish), and 'fr' (french)
+game.Players.PlayerAdded:Connect(function(player)
+    player.Chatted:Connect(function(messsage)
+        local xd = messsage:lower()
+        local message = messsage:gsub(" ", "+") 
+        print(message)  
+        local xd = game:HttpGet('https://normal-api.ml/translate?text='..message..'&to='..language..'')
+	    local data = game.HttpService:JSONDecode(xd)
+	    local res = data.translated
+        print(res)
+        rconsoleprint("["..player.Name.."]: "..res.." (translated to english)")
+    end)
+end)
+for i, player in pairs(game.Players:GetPlayers()) do
+    player.Chatted:Connect(function(messsage)
+        local xd = messsage:lower()
+        local message = messsage
+            print(message)  
+            local xd = game:HttpGet('https://normal-api.ml/translate?text='..message..'&to='..language..'')
+	        local data = game.HttpService:JSONDecode(xd)
+	        local res = data.translated
+	        print(res)
+	         rconsoleprint("["..player.Name.."]: "..res.." (translated to english)")
+    end)
+end
